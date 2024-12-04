d943955bd9e2597e0efc19c0c74a73e6069dcf31
Olena.Holub <olehol@alu.edu.gva.ua>
2024-12-04 20:01:39 +0100

    add a4

---
+++ a4_laravel_relationships/resources/views/welcome.blade.php
@@ -0,0 +1,29 @@
+@include('topbar')
+
+<div>
+    @foreach ($noticias as $noticia)
+        <div style="display: grid; grid-template-columns: 1em 1fr; gap: 0.5em; align-items: center">
+            <form method=POST action="/vote/{{ $noticia->id }}" style="margin: 0; display: inline">
+                @csrf
+                <a><input type="submit" class="triangle" value="bbb"/></a>
+            </form>
+            
+            <div>
+                <a href="{{ $noticia->enlace }}">{{ $noticia->titulo }}</a>
+            </div>
+            <div></div>
+            <div>
+                <p class="graytext">
+                    {{ count($noticia->votos ?? []) }} votes 
+                    by {{ $noticia->user->name }} {{ $noticia->created_at->toFormattedDateString() }} 
+                    | <a href="/noticia/{{$noticia->id}}">{{ count($noticia->comentarios ?? []) }} comentarios</a>
+                </p>
+            </div>
+            
+        </div>
+    @endforeach
+
+    @if ($noticias->hasMorePages()) 
+        <a href="{{ $noticias->nextPageUrl() }}" rel="next">More</a> 
+    @endif
+    </div>
