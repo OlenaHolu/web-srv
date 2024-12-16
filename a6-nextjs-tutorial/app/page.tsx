76081a6c7efa3375ee81414db414c39fd9f5e483
Olena.Holub <olehol@alu.edu.gva.ua>
2024-12-16 15:23:55 +0100

    add image, add ignoreBuildWErrors to next.config.ts

--- a6-nextjs-tutorial/app/page.tsx
+++ a6-nextjs-tutorial/app/page.tsx
@@ -1,4 +1,6 @@
+import Image from "next/image";
 import EstadoFactura from "./ui/EstadoFactura";
+import { lusitana, inter, fuenteX } from '@/app/ui/fonts';
 export default function Home() {
   return (
     <main>
@@ -6,6 +8,24 @@ export default function Home() {
       <EstadoFactura estado="pagada" />
       <EstadoFactura estado="pendiente" />
       <EstadoFactura estado="facturado" />
+      <p className={`${inter.className} antialiased`}>Chapter 3 - Font 1</p>
+      <p className={`${lusitana.className} antialiased`}>Chapter 3 - Font 2</p>
+      <p className={`${fuenteX.className} antialiased`}>Chapter 3 - Font 3</p>
+      
+      <Image 
+        src="/2.jpg"
+        width={1000}
+        height={760}
+        className="hidden md:block"
+        alt="image desktop version"
+      />
+       <Image 
+        src="/3.jpg"
+        width={560}
+        height={620}
+        className="block md:hidden"
+        alt="image mobile version"
+      />
     </main>
   );
 }
